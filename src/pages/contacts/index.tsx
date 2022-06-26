47bde1ea37223ad0ce4e70383206918a9e048705
imdimasan <info@starlink.by>
2022-06-26 14:48:10 +0300

    AUTONEWART-02 Files and folders structure

---
+++ src/pages/contacts/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact page</title>
+      </Head>
+
+      <h1>Contacts page</h1>
+    </>
+  );
+};
+
+export default Contacts;
